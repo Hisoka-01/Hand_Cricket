86580d39c8e1967b01a04a24f4d427d1ef1cf1bd
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2024-11-10 12:06:38 +0530

    Add files via upload

---
+++ Frontend/1V1/Battle/script.js
@@ -0,0 +1,58 @@
+// var sign1 = document.getElementById('signsImg1');
+// var sign2 = document.getElementById('signsImg2');
+
+
+
+// // propAdd (sign1, "one.png", "leftONE")
+// // propAdd (sign2, "one.png", "rightONE")
+
+
+// var one = document.getElementById('one')
+// var two = document.getElementById('two')
+// var three = document.getElementById('three')
+// var four = document.getElementById('four')
+// var five = document.getElementById('five')
+// var six = document.getElementById('six')
+
+// one.addEventListener('click', () => {
+//     sign1.src="one.png"
+//     sign1.classList.add("leftONE")
+//     console.log("Worked");
+// })
+// two.addEventListener('click', () => {
+//     sign1.src="two.png"
+//     sign1.classList.add("leftTWO")
+//     console.log("Worked");
+// })
+// three.addEventListener('click', () => {
+//     sign1.src="three.png"
+//     sign1.classList.add("leftTHREE")
+//     console.log("Worked");
+// })
+// four.addEventListener('click', () => {
+//     sign1.src="four.png"
+//     sign1.classList.add("leftFOUR")
+//     console.log("Worked");
+// })
+// five.addEventListener('click', () => {
+//     sign1.src="five.png"
+//     sign1.classList.add("leftFIVE")
+//     console.log("Worked");
+// })
+// six.addEventListener('click', () => {
+//     sign1.src="six.png"
+//     sign1.classList.add("leftSIX")
+//     console.log("Worked");
+// })
+
+
+
+
+
+
+
+
+
+
+
+
